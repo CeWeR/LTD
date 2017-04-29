a147256fd06c99c6ee06c8d1755cffbec2c68306
Morteza1615 <mori13796@gmail.com>
2017-04-29 05:56:33 -0700

    morteza1615

--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
 bot_token = "285985426:AAEzkoxPQkHCkZNIXp2TA3O4up3Rd-yH8qw"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "1.0"
-admingp = -191128096,351963104
+admingp = -191128096
+
